d67920bba7a656c95b1aa314adddaa865b5f0d42
marcus <marcus@3ea2da762853>
2023-09-25 13:34:51 +0000

    fixed module info module name

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@
  * The fedora module provides interfaces and classes to use a Fedora Commons System in a Cora based
  * system.
  */
-module se.uu.ub.cora.fedoraarchive {
+module se.uu.ub.cora.tikacontentanalyzer {
 	// requires se.uu.ub.cora.storage;
 	// requires se.uu.ub.cora.initialize;
 	// requires se.uu.ub.cora.fedora;
