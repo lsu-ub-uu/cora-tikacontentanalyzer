37512570b310340548f728c6578f9a59de9eccd2
pere <pere@2a786f92ca73>
2023-09-25 13:23:52 +0000

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,19 @@
+/**
+ * The fedora module provides interfaces and classes to use a Fedora Commons System in a Cora based
+ * system.
+ */
+module se.uu.ub.cora.fedoraarchive {
+	// requires se.uu.ub.cora.storage;
+	// requires se.uu.ub.cora.initialize;
+	// requires se.uu.ub.cora.fedora;
+	// requires se.uu.ub.cora.converter;
+	// requires se.uu.ub.cora.httphandler;
+	// requires se.uu.ub.cora.xmlconverter;
+	// requires se.uu.ub.cora.logger;
+	//
+	// provides se.uu.ub.cora.storage.archive.RecordArchiveProvider with
+	// FedoraRecordArchiveProvider;
+	// provides se.uu.ub.cora.storage.archive.ResourceArchiveInstanceProvider
+	// with FedoraResourceArchiveProvider;
+
+}
