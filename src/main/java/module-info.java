0e1ac3d3dd8727ea6aba8008af540480a26ce43a
perebartrolisimo <pere.bartroli@ub.uu.se>
2023-09-26 16:01:03 +0000

    CORA-2265 (CORA-2266) configure service loader

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,5 @@
+import se.uu.ub.cora.tikacontentanalyzer.TikaContentAnalyzerProvider;
+
 /**
  * The fedora module provides interfaces and classes to use a Fedora Commons System in a Cora based
  * system.
@@ -6,17 +8,8 @@ module se.uu.ub.cora.tikacontentanalyzer {
 	requires se.uu.ub.cora.contentanalyzer;
 	requires org.apache.tika.core;
 	requires org.apache.commons.io;
-	// requires se.uu.ub.cora.storage;
-	// requires se.uu.ub.cora.initialize;
-	// requires se.uu.ub.cora.fedora;
-	// requires se.uu.ub.cora.converter;
-	// requires se.uu.ub.cora.httphandler;
-	// requires se.uu.ub.cora.xmlconverter;
-	// requires se.uu.ub.cora.logger;
-	//
-	// provides se.uu.ub.cora.storage.archive.RecordArchiveProvider with
-	// FedoraRecordArchiveProvider;
-	// provides se.uu.ub.cora.storage.archive.ResourceArchiveInstanceProvider
-	// with FedoraResourceArchiveProvider;
+
+	provides se.uu.ub.cora.contentanalyzer.ContentAnalyzerInstanceProvider
+			with TikaContentAnalyzerProvider;
 
 }
