cd243001169ecec968b9d5c48572c4dcd0ee1cb2
olovm <olov.mckie@ub.uu.se>
2023-09-28 11:50:36 +0000

    adding java.sql to module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,6 +8,8 @@ module se.uu.ub.cora.tikacontentanalyzer {
 	requires se.uu.ub.cora.contentanalyzer;
 	requires org.apache.tika.core;
 	requires org.apache.commons.io;
+	// java.sql needed by TikaInputStream
+	requires java.sql;
 
 	provides se.uu.ub.cora.contentanalyzer.ContentAnalyzerInstanceProvider
 			with TikaContentAnalyzerProvider;
--- src/main/java/se/uu/ub/cora/tikacontentanalyzer/TikaContentAnalyzer.java
+++ src/main/java/se/uu/ub/cora/tikacontentanalyzer/TikaContentAnalyzer.java
@@ -27,7 +27,6 @@ import se.uu.ub.cora.contentanalyzer.ContentAnalyzer;
 import se.uu.ub.cora.contentanalyzer.ContentAnalyzerException;
 
 public class TikaContentAnalyzer implements ContentAnalyzer {
-
 	private static final String DETECTION_ERROR_MESSAGE = "Failed to detect mimetype from resource: ";
 	private Tika tika;
 
